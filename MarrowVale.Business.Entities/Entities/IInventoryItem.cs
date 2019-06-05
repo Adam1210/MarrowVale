7e64a987d86b21a853622bf72c641b8262adcf38
Craig A Wiswell <cwiswell63@gmail.com>
2019-06-04 23:07:35 -0400

    Add properties to various entities
    Create IInventoryItem interface that has property of name. This is so inventory can maintain a list of items in the inventory.

---
+++ MarrowVale.Business.Entities/Entities/IInventoryItem.cs
@@ -0,0 +1,8 @@
+﻿
+namespace MarrowVale.Business.Entities.Entities
+{
+    public interface IInventoryItem
+    {
+        string Name { get; set; }
+    }
+}
--- MarrowVale.Business.Entities/Entities/Spell.cs
+++ MarrowVale.Business.Entities/Entities/Spell.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace MarrowVale.Business.Entities.Entities
 {
     public class Spell
     {
+        public string Name { get; protected set; }
 
+        // Not sure how spells going to work. Is it Mana based or some other resource?
+        public int Cost { get; protected set; }
     }
 }
