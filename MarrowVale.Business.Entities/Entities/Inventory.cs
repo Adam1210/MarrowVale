4dc4270e852aacac1dc1b23a1470b1c318b13056
Craig A Wiswell <cwiswell63@gmail.com>
2019-06-04 23:07:35 -0400

    Add properties to various entities
    Create IInventoryItem interface that has property of name. This is so inventory can maintain a list of items in the inventory.

--- MarrowVale.Business.Entities/Entities/Inventory.cs
+++ MarrowVale.Business.Entities/Entities/Inventory.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace MarrowVale.Business.Entities.Entities
 {
     public class Inventory
     {
+        public int Size { get; set; }
     }
 }
