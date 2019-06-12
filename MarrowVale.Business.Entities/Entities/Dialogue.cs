727f0c7f5b6905095ee742a6dcfcbcd85665aa89
Craig A Wiswell <cwiswell63@gmail.com>
2019-06-12 17:11:32 -0400

    Add beginnings of a Dialogue class

---
+++ MarrowVale.Business.Entities/Entities/Dialogue.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace MarrowVale.Business.Entities.Entities
+{
+    public class Dialogue
+    {
+        public string Type { get; set; }
+        public string TriggerText { get; set; }
+        public bool AlreadySpoken { get; set; }
+        public IList<Dialogue> Dialogues { get; set; }
+
+    }
+}
