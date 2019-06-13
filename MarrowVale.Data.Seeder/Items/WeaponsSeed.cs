cb6e4436530e2a2f00c5cb5bcf4cc3e1e8223b3e
Craig A Wiswell <cwiswell63@gmail.com>
2019-06-13 18:41:43 -0400

    Add Data Seed project

---
+++ MarrowVale.Data.Seeder/Items/WeaponsSeed.cs
@@ -0,0 +1,18 @@
+﻿using MarrowVale.Business.Entities.Entities;
+
+namespace MarrowVale.Data.Seeder.Items
+{
+    public static class WeaponsSeed
+    {
+        public static Weapon GetWoodenSword()
+        {
+            var woodenSword = new Weapon(1, Business.Entities.Enums.WeaponTypeEnum.Sword);
+
+            woodenSword.Damage = 1;
+            woodenSword.Name = "Wooden Sword";
+            woodenSword.Description = "Fragile piece of wood that comes to a point";
+
+            return woodenSword;
+        }
+    }
+}
---
+++ MarrowVale.Data.Seeder/Npcs/VillagersSeed.cs
@@ -0,0 +1,14 @@
+﻿using MarrowVale.Business.Entities.Entities;
+
+namespace MarrowVale.Data.Seeder.Npcs
+{
+    public static class VillagersSeed
+    {
+        public static Npc GetVillager()
+        {
+            var villager = new Npc();
+
+            return villager;
+        }
+    }
+}
