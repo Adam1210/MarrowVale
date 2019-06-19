ea60873a797d3bae4f0981e14c1f82728eee0b40
Craig A Wiswell <cwiswell63@gmail.com>
2019-06-18 20:35:21 -0400

    Remove unnecessary using statements

--- MarrowVale.Business.Entities/Entities/Time.cs
+++ MarrowVale.Business.Entities/Entities/Time.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace MarrowVale.Business.Entities.Entities
+﻿namespace MarrowVale.Business.Entities.Entities
 {
     public class Time
     {
--- MarrowVale.Business.Entities/Enums/TimeEnum.cs
+++ MarrowVale.Business.Entities/Enums/TimeEnum.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace MarrowVale.Business.Entities.Enums
+﻿namespace MarrowVale.Business.Entities.Enums
 {
     public enum TimeEnum
     {
